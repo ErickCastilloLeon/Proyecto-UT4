af1670fa774612b899b921e10b8108b4384be112
ErickCastilloLeon <149406911+ErickCastilloLeon@users.noreply.github.com>
2024-02-07 20:46:05 +0100

    1.3
    Avansando en el footer , lo de contacto , cophyright y soporte

--- js/Es_Mode/CoWiReSo.js
+++ js/Es_Mode/CoWiReSo.js
@@ -1,22 +1,5 @@
 const images = [
     { src: "https://media.tenor.com/RcX3hUY425kAAAAj/toothless-dragon-toothless.gif", alt: "Imagen 1", title: ":3", link: "html2.html" }, // Para que no se quede vacio
-    { src: "https://media.tenor.com/RcX3hUY425kAAAAj/toothless-dragon-toothless.gif", alt: "Imagen 2", title: ":3", link: "html2.html" },
-    { src: "https://media.tenor.com/RcX3hUY425kAAAAj/toothless-dragon-toothless.gif", alt: "Imagen 3", title: ":3", link: "html2.html" },
-    { src: "https://media.tenor.com/RcX3hUY425kAAAAj/toothless-dragon-toothless.gif", alt: "Imagen 4", title: ":3", link: "html2.html" },
-    { src: "https://media.tenor.com/RcX3hUY425kAAAAj/toothless-dragon-toothless.gif", alt: "Imagen 5", title: ":3", link: "html2.html" },
-    { src: "https://media.tenor.com/RcX3hUY425kAAAAj/toothless-dragon-toothless.gif", alt: "Imagen 5", title: ":3", link: "html2.html" },
-    { src: "https://media.tenor.com/RcX3hUY425kAAAAj/toothless-dragon-toothless.gif", alt: "Imagen 6", title: ":3", link: "html2.html" },
-    { src: "https://media.tenor.com/RcX3hUY425kAAAAj/toothless-dragon-toothless.gif", alt: "Imagen 7", title: ":3", link: "html2.html" },
-    { src: "https://media.tenor.com/RcX3hUY425kAAAAj/toothless-dragon-toothless.gif", alt: "Imagen 8", title: ":3", link: "html2.html" },
-    { src: "https://media.tenor.com/RcX3hUY425kAAAAj/toothless-dragon-toothless.gif", alt: "Imagen 9", title: ":3", link: "html2.html" },
-    { src: "https://media.tenor.com/RcX3hUY425kAAAAj/toothless-dragon-toothless.gif", alt: "Imagen 8", title: ":3", link: "html2.html" },
-    { src: "https://media.tenor.com/RcX3hUY425kAAAAj/toothless-dragon-toothless.gif", alt: "Imagen 7", title: ":3", link: "html2.html" },
-    { src: "https://i.pinimg.com/originals/2b/3f/a3/2b3fa38cd997f90a273736fa4d2115f0.gif", alt: "Imagen 6", title: ":3", link: "html2.html" },
-    { src: "https://i.pinimg.com/originals/2b/3f/a3/2b3fa38cd997f90a273736fa4d2115f0.gif", alt: "Imagen 5", title: ":3", link: "html2.html" },
-    { src: "https://i.pinimg.com/originals/2b/3f/a3/2b3fa38cd997f90a273736fa4d2115f0.gif", alt: "Imagen 4", title: ":3", link: "html2.html" },
-    { src: "https://i.pinimg.com/originals/2b/3f/a3/2b3fa38cd997f90a273736fa4d2115f0.gif", alt: "Imagen 3", title: ":3", link: "html2.html" },
-    { src: "https://i.pinimg.com/originals/2b/3f/a3/2b3fa38cd997f90a273736fa4d2115f0.gif", alt: "Imagen 2", title: ":3", link: "html2.html" },
-    { src: "https://i.pinimg.com/originals/2b/3f/a3/2b3fa38cd997f90a273736fa4d2115f0.gif", alt: "Imagen 1", title: ":3", link: "html2.html" }, // muchas repeticiones pero me ahorra al tener que editar despues
 ];
 // La cantidad de imágenes que se msotraran por pagina
 const imagesPerPage = 8;
