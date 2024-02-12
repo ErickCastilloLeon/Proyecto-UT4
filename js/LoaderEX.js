683d6daf6a84f86826da0d85089eae256d39502d
ErickCastilloLeon <149406911+ErickCastilloLeon@users.noreply.github.com>
2024-02-12 15:17:46 +0100

    1.5
    Se cambio el nombre del JS CargaScript por Loader , en ves tener el rol de cargar todos los contenidos que se le solicita , ahora podra cargar X contenido solicitado , no obstante esta el error de que solo llega a cargar 1-2

---
+++ js/LoaderEX.js
@@ -0,0 +1,62 @@
+function  CargarSeAJap(script) {
+    var scriptElement = document.createElement('script');
+    scriptElement.src = script;
+    document.head.appendChild(scriptElement);
+}
+
+function  CargarSeAEn(script) {
+    var scriptElement = document.createElement('script');
+    scriptElement.src = script;
+    document.head.appendChild(scriptElement);
+}
+
+function  CargarSeAEs(script) {
+    var scriptElement = document.createElement('script');
+    scriptElement.src = script;
+    document.head.appendChild(scriptElement);
+}
+
+function  CargarAJap(script) {
+    var scriptElement = document.createElement('script');
+    scriptElement.src = script;
+    document.head.appendChild(scriptElement);
+}
+
+function  CargarAEs(script) {
+    var scriptElement = document.createElement('script');
+    scriptElement.src = script;
+    document.head.appendChild(scriptElement);
+}
+
+function  CargarNoLiWeNo(script) {
+    var scriptElement = document.createElement('script');
+    scriptElement.src = script;
+    document.head.appendChild(scriptElement);
+}
+
+function  CargarNoLiNoLi(script) {
+    var scriptElement = document.createElement('script');
+    scriptElement.src = script;
+    document.head.appendChild(scriptElement);
+}
+
+
+
+
+
+/*
+function cargarScriptCoWiDoEs(script) {
+    // Eliminar el script anterior si existe
+    var existingScript = document.querySelector('script[src="' + script + '"]');
+    if (existingScript) {
+        existingScript.parentNode.removeChild(existingScript);
+    }
+
+    // Crear y agregar el nuevo script
+    var scriptElement = document.createElement('script');
+    scriptElement.src = script;
+    document.head.appendChild(scriptElement);
+}
+
+ */
+
